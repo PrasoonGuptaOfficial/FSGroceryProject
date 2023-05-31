d905273b99123bdab2b30126405f8d315cba8e68
Prasoon Gupta <prasoonguptaofficialwork@gmail.com>
2023-06-01 03:36:17 +0530

    Added Testing Library Initialization

--- Frontend/mobile/__tests__/App-test.tsx
+++ Frontend/mobile/__tests__/App-test.tsx
@@ -7,8 +7,10 @@ import React from 'react';
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import {render} from '@testing-library/react-native';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('App rendering Demo', () => {
+  it('Initial App Render', () => {
+    render(<App />);
+  });
 });
